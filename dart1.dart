26b2f0e789032283492bfb4c29b7d8f8d25a97c8
kin-yao <preachert898@gmail.com>
2024-02-29 17:33:23 +0300

    dart lists....found out they are just like arrays in python

---
+++ dart1.dart
@@ -0,0 +1,26 @@
+//void main() {
+// String name = "John";
+// String address = "Kenya";
+// num age = 20;
+// num height = 5.9;
+// bool isMarried = false;
+//void
+//print("Name is $name");
+//print("Address is $address");
+//print("Age is $age");
+//print("Height is $height");
+//print("Married status is $isMarried");
+//}
+
+// list datatype
+void main() {
+  List my_list = ["Mexico", "Columbia", "Cuba"];
+
+  print("The countries in the list are $my_list");
+
+  print("Value of my_list[0] is ${my_list[0]}"); // index 0
+  print("Value of my_list[1] is ${my_list[1]}"); // index 1
+  print("Value of my_list[2] is ${my_list[2]}"); // index 2
+
+  print(my_list);
+}
