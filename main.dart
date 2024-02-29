fc38c30d6e660e2fae99b80929a5208c63af3bc9
kin-yao <preachert898@gmail.com>
2024-02-29 16:51:15 +0300

    first dart commit

--- main.dart
+++ main.dart
@@ -1,16 +1,13 @@
 void main() {
-String name = "John";
-String address = "Kenya";
-num age = 20;
-num height =5.9;
-bool isMarried = false;
-double weight  = 66.7;
+  String name = "John";
+  String address = "Kenya";
+  num age = 20;
+  num height = 5.9;
+  bool isMarried = false;
 
-
-print("Name is $name");
-print("Address is $address");
-print("Age is $age");
-print("Height is $height");
-print("Married status is $isMarried");
-print("Weight is $weight");
+  print("Name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print("Height is $height");
+  print("Married status is $isMarried");
 }
